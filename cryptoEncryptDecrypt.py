deaedc087ae87670d99c1e848c6e2251525ea74b
David-Burton-tcd <burtond@tcd.ie>
2023-11-11 20:38:59 +0000

    Added import&export functionality for keys

--- cryptoEncryptDecrypt.py
+++ cryptoEncryptDecrypt.py
@@ -5,7 +5,8 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.serialization import Encoding, PrivateFormat, NoEncryption, load_pem_private_key, load_pem_public_key
 import cryptography
 
 class CryptographicSolution():
@@ -57,6 +58,34 @@ class CryptographicSolution():
             )
         )
         return plaintext
+    
+    def exportKey(prefix, privateKey, publicKey):
+
+        binaryPrivateKey = privateKey.private_bytes(
+            encoding = serialization.Encoding.PEM,
+            format = serialization.PrivateFormat.PKCS8,
+            encryption_algorithm = serialization.NoEncryption()
+        )
+        with open(prefix + '_privateKey.bin', 'wb') as f:
+            f.write(binaryPrivateKey)
+        
+        binaryPublicKey = publicKey.public_bytes(
+            encoding = serialization.Encoding.PEM,
+            format = serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+
+        with open(prefix + '_publicKey.bin', 'wb') as f:
+            f.write(binaryPublicKey)
+
+    def importKey(keyFile, format):
+        with open(keyFile, 'rb') as pemIn:
+            pemRead = pemIn.read()
+
+        if(format == 'private'):
+            returnKey = load_pem_private_key(pemRead, None, default_backend())
+        if(format == 'public'):
+            returnKey = load_pem_public_key(pemRead)
+        return returnKey
 
 
 ### TESTING ###
@@ -71,9 +100,9 @@ sig2 = CryptographicSolution.sign(msg, pvt2) # signed with *other* private key
 # print(sig1)
 
 res = CryptographicSolution.verify(msg, sig1, pub1)
-print(res)                    # True
+print(res) #True
 res = CryptographicSolution.verify(msg, sig2, pub1)
-print(res)                    # False
+print(res) #False
 
 testr = CryptographicSolution.encrypt(b'Hello', pub1)
 print(sig1)
@@ -81,3 +110,25 @@ print(testr)
 
 reTestr = CryptographicSolution.decrypt(testr, pvt1)
 print(reTestr)
+
+CryptographicSolution.exportKey('device1', pvt1, pub1)
+publicImport = CryptographicSolution.importKey('device1_publicKey.bin', 'public')
+privateImport = CryptographicSolution.importKey('device1_privateKey.bin', 'private')
+
+print(publicImport.public_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PublicFormat.SubjectPublicKeyInfo))
+
+print(privateImport.private_bytes(
+    encoding = serialization.Encoding.PEM,
+    format = serialization.PrivateFormat.PKCS8,
+    encryption_algorithm = serialization.NoEncryption()
+))
+
+testMessage = b"Testing my imported key"
+sig3 = CryptographicSolution.sign(testMessage, privateImport) # signed with private key
+res = CryptographicSolution.verify(testMessage, sig3, publicImport)
+print(res) #True
+
+
+# , default_backend()
